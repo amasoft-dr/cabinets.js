c72be05b010f3e266871b9746872bb479fe2f81c
andreidimjg <andrei.dim.jg@gmail.com>
2021-02-08 21:58:42 +0000

    Testing specific interceptors in complex Store

--- src/tests/cabinets.advanced.test.js
+++ src/tests/cabinets.advanced.test.js
@@ -1,13 +1,28 @@
 import { setupStore, useStore } from "../module.js";
 
+class LocalStorage {
+    constructor(){
+        this.data = {}
+    }
+    set(name, value){
+        this.data[name] = JSON.stringify(value) ;
+    }
 
-const strId = (str) => [...str].map(c => c.charCodeAt(0) )
-                          .join("") + "_" + new Date().getTime();
+    get(name){
+      return this.data[name];
+    }
+
+};
+
+const localStorage = new LocalStorage();
+
+const strId = (str) => [...str].map(c => c.charCodeAt(0))
+    .join("") + "_" + new Date().getTime();
 
 const blogStore = {
     name: "blogStore",
     initState: {
-        blog:{
+        blog: {
             comments: [],
             articles: []
         }
@@ -15,37 +30,92 @@ const blogStore = {
     operations: {
         addComment: (state, newComment) => {
             state.blog.comments = [...state.blog.comments, newComment];
-            return {...state};
+            return { ...state };
         },
         removeComment: (state, commentId) => {
-            state.blog.comments = 
-                     state.blog.comments.filter( comment => comment.id != commentId);
-            return {...state};
+            state.blog.comments =
+                state.blog.comments.filter(comment => comment.id !== commentId);
+            return { ...state };
         },
         addArticle: (state, newArticle) => {
             state.blog.articles = [...state.blog.articles, newArticle];
-            return {...state};
+            return { ...state };
         },
         removeComment: (state, articleId) => {
-             state.blog.articles = 
-                     state.blog.articles.filter( article => article.id != articleId);
-            return {...state};
+            state.blog.articles =
+                state.blog.articles.filter(article => article.id != articleId);
+            return { ...state };
         }
-       
+
     },
-    maps:{
-     //Adding a default map to modify the payload adding id to both comments and articles
-    //Adding creation date.
-     def: (state, entry) => {
-         const id = strId(entry.text);
-         return {...entry, id, creationDate: new Date() };
-     }
+    maps: {
+        //Adding a default map to modify the payload adding id to both comments and articles
+        //Adding creation date.
+        def: (state, entry) => {
+            const id = strId(entry.author);
+            return { ...entry, id, creationDate: new Date() };
+        }
+    },
+    interceptors:{
+        addArticle: (state, payload)=> {
+            localStorage.set("article_backups", {state, payload, lastUpdate:new Date()});
+        }
     }
-}; 
+};
 
 
 it("Setting Up and Finding Complex Store", () => {
-  const store = setupStore(blogStore);
-  const foundStore = useStore("blogStore");
-  expect(store).toStrictEqual(foundStore);
-}); 
+    const store = setupStore(blogStore);
+    const foundStore = useStore("blogStore");
+    expect(store).toStrictEqual(foundStore);
+});
+
+it("Changes Complex Store's State", () => {
+    const { fire, actions, getState } = useStore("blogStore");
+    const article = {
+        text: "Hello World to everybody",
+        title: "Hello World!",
+        author: "@amasoft"
+    }
+    fire(actions.addArticle(article));
+    const article1 = getState().blog.articles[0];
+    expect(article.text).toBe(article1.text);
+
+});
+
+it("Checks if default map was executed in Complex Store", () => {
+    const { fire, actions, getState } = useStore("blogStore");
+    const article = {
+        text: "This is Dimi from Amasoft",
+        title: "Hello from Amasoft!",
+        author: "@andreidim"
+    }
+
+    fire(actions.addArticle(article));
+    const article1 = getState().blog.articles
+        .find(article => article.author === "@andreidim");
+
+    expect(article1).toHaveProperty("id");
+    expect(article1).toHaveProperty("creationDate");
+
+    const comment = {
+        text: "Thank you @andreidim", author: "@adpmaster",
+        articleId: article1.id
+    }
+
+    fire(actions.addComment(comment));
+    const comment1 = getState().blog.comments
+        .find(comment => comment.author === "@adpmaster");
+
+    expect(comment1).toHaveProperty("id");
+    expect(comment1).toHaveProperty("creationDate");
+
+});
+
+
+it("Checks if action's interceptor is executing in Complex Store", () => {
+    //Hence we defined an interceptor to backup always our blog state, payload
+    //and it also adds a lastUpdate field, let's see our fake localStorage.
+    const blogBackup = JSON.parse(localStorage.get("article_backups") );
+    expect(blogBackup).toHaveProperty("lastUpdate");
+});
